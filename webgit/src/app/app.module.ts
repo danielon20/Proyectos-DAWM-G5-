d4115e6985bcf30e704cc3f7f22aef79af6c65b4
DarinkaTownsend <66121094+DarinkaTownsend@users.noreply.github.com>
2021-07-14 10:37:55 -0500

    inicio y cursos

--- webgit/src/app/app.module.ts
+++ webgit/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { SharedModule } from './shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { ContactosComponent } from './pages/contactos/contactos.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NoticiasComponent,
-    ContactosComponent
+    ContactosComponent,
+    CursosComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
