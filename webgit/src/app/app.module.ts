6adbe8424a5ca9b8b38e0db602633c96c4e810b0
Tommy Beltrán <tomdbelt@espol.edu.ec>
2021-07-14 10:15:13 -0500

    Componentes para sección noticias y contactos creado. Separado en pages

--- webgit/src/app/app.module.ts
+++ webgit/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
