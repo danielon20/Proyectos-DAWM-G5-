d848ae34134c039d8015a3a52b3f8a8b93207681
Tommy Beltrán <tomdbelt@espol.edu.ec>
2021-07-14 11:10:42 -0500

    Navegación entre ventanas Noticias y Contactos aplicada

--- webgit/src/app/app-routing.module.ts
+++ webgit/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"noticias", component:NoticiasComponent},
+  { path:"contactos", component:ContactosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
