f7849d96afe99fcb3c3f2ad5f7e24b11bd18441b
DarinkaTownsend <66121094+DarinkaTownsend@users.noreply.github.com>
2021-07-14 17:33:37 -0500

    arreglado

--- webgit/src/app/shared/shared.module.ts
+++ webgit/src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    ToolbarComponent
-  ],
+    HeaderComponent],
   imports: [
     CommonModule
   ],
