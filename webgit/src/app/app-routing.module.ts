28d268b7a2868be858846eab8eacbd70c3c734cc
Raul Villao <rjvillao@espol.edu.ec>
2021-07-14 14:22:36 -0500

    Solución de problema en inicio
    Se borro el div del loading para que no haga error en el header

--- webgit/src/app/app-routing.module.ts
+++ webgit/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactosComponent } from './pages/contactos/contactos.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { EquipoDesarrolloComponent } from './pages/equipo-desarrollo/equipo-desarrollo.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 const routes: Routes = [
   {path:"noticias", component:NoticiasComponent},
   {path:"contactos", component:ContactosComponent},
-  {path:"equipo", component:EquipoDesarrolloComponent}
-
+  {path:"equipo", component:EquipoDesarrolloComponent},
+  {path:"inicio", component:InicioComponent}
 ];
 
 @NgModule({
