afd688573b455a258997d24f9a2fb7086bbeee9a
Tommy Beltrán <tomdbelt@espol.edu.ec>
2021-07-15 12:15:23 -0500

    Noticias implementadas (local)

--- webgit/src/app/pages/noticias/noticias.component.ts
+++ webgit/src/app/pages/noticias/noticias.component.ts
@@ -41,6 +41,116 @@ export class NoticiasComponent implements OnInit {
   }
   */
   constructor() { }
+  news:any[] = [
+    {
+      "title":"Cómo eliminar automáticamente un pantallazo de WhatsApp después de haberlo enviado",
+      "link":"https://eluniverso.com/larevista/tecnologia/como-eliminar-automaticamente-un-screenshot-de-whatsapp-despues-de-haberlo-enviado-nota/",
+      "creator":"Carla Rodríguez",
+      "pubDate":"Wed, 14 Jul 2021 20:57:47 +0000",
+      "description":"Gracias a este truco, los pantallazos de WhatsApp o cualquier otra aplicación se borrarán solos, luego de que pase un tiempo elegido por ti."
+    },
+    {
+      "title":"Las cuentas gratuitas de Google Meet solo podrán realizar videollamadas de hasta 60 minutos",
+      "link":"https://eluniverso.com/larevista/tecnologia/las-cuentas-gratuitas-de-google-meet-solo-podran-realizar-videollamadas-de-hasta-60-minutos-nota/",
+      "creator":"Andres Alejandro",
+      "pubDate":"Tue, 13 Jul 2021 15:45:16 +0000",
+      "description":"La herramienta permitía hacer videollamadas en grupo de hasta 24 horas, como las cuentas profesionales."
+    },
+    {
+      "title":"La tecnología OLED se toma las pantallas del mundo",
+      "link":"https://eluniverso.com/larevista/tecnologia/la-tecnologia-oled-se-toma-las-pantallas-del-mundo-nota/",
+      "creator":"Jaime Freire García",
+      "pubDate":"Tue, 13 Jul 2021 11:00:00 +0000",
+      "description":"La tecnología de pixeles autoiluminados marca la diferencia en la experiencia de visualización."
+    },
+    {
+      "title":"Microsoft presenta nueva opción para pymes",
+      "link":"https://eluniverso.com/larevista/tecnologia/microsoft-presenta-nueva-opcion-para-pymes-nota",
+      "creator":"Jaime Freire García",
+      "pubDate":"Tue, 13 Jul 2021 11:00:00 +0000",
+      "description":"Power Platform, de Microsoft, es un paquete de herramientas 'low-code' que permite a todos desarrollar aplicaciones y automatizaciones para acelerar su trabajo."
+    },
+    {
+      "title":"“FIFA 22” sale el 1 de octubre, usará tecnología con “más realismo”",
+      "link":"https://eluniverso.com/entretenimiento/videojuegos/fifa-22-sale-el-1-de-octubre-usara-tecnologia-con-mas-realismo-nota/",
+      "creator":"Rosa Falconi Johnson",
+      "pubDate":"Mon, 12 Jul 2021 18:06:57 +0000",
+      "description":"Kylian Mbappé en la portada. la nueva tecnología patentada de aprendizaje automático brindará la experiencia de fútbol más realista, fluida y receptiva."
+    },
+    {
+      "title":"Si no le gusta Windows 11, Microsoft le dará 10 días para regresar a Windows 10",
+      "link":"https://eluniverso.com/larevista/tecnologia/si-no-le-gusta-windows-11-microsoft-le-dara-10-dias-para-regresar-a-windows-10-nota/",
+      "creator":"Andres Alejandro",
+      "pubDate":"Mon, 12 Jul 2021 17:47:17 +0000",
+      "description":"La posibilidad de actualizar a Windows 11 dependerá de las características del ordenador."
+    },
+    {
+      "title":"Gato gigante en 3D causa asombro entre los habitantes de Tokio",
+      "link":"https://eluniverso.com/larevista/tecnologia/gato-gigante-en-3d-causa-asombro-entre-los-habitantes-de-tokio-nota/",
+      "creator":"Andres Alejandro",
+      "pubDate":"Mon, 12 Jul 2021 14:20:42 +0000",
+      "description":"El felino de tonos blancos, negros y naranjas puede ser visto entre dos anuncios."
+    },
+    {
+      "title":"Instagram priorizará videos sobre las fotos para intentar competir contra TikTok",
+      "link":"https://eluniverso.com/larevista/tecnologia/instagram-priorizara-videos-sobre-las-fotos-para-intentar-competir-contra-tiktok-nota/",
+      "creator":"David Muñoz",
+      "pubDate":"Mon, 12 Jul 2021 14:00:00 +0000",
+      "description":">Con estos cambios anunciados, Instagram desea distanciarse de su enfoque anterior de solo ser una aplicación para compartir fotos."
+    },
+    {
+      "title":"Cómo saber si tu celular ha sido hackeado y qué puedes hacer para evitarlo",
+      "link":"https://eluniverso.com/larevista/tecnologia/como-saber-si-tu-celular-ha-sido-hackeado-y-que-puedes-hacer-para-evitarlo-nota/",
+      "creator":"Carla Rodríguez",
+      "pubDate":"Sat, 10 Jul 2021 01:00:00 +0000",
+      "description":"¿Te dura muy poco la batería del teléfono? ¿Consumes tus datos en pocos días?"
+    },
+    {
+      "title":"Las 9 aplicaciones que robaban contraseñas y que Google las eliminó de su tienda",
+      "link":"https://eluniverso.com/larevista/tecnologia/las-9-aplicaciones-que-robaban-contrasenas-y-que-google-las-elimino-de-su-tienda-nota/",
+      "creator":"David Muñoz",
+      "pubDate":"Mon, 12 Jul 2021 15:21:39 +0000",
+      "description":"Dr. Web alertó que, aunque ya han sido eliminadas de Google Play, aún siguen estando disponibles en plataformas de terceros y agregadores de apps."
+    },
+    {
+      "title":"Microsoft pide a usuarios instalar actualización de forma inmediata tras problemas de seguridad",
+      "link":"https://eluniverso.com/larevista/tecnologia/microsoft-pide-a-usuarios-instalar-actualizacion-de-forma-inmediata-tras-problemas-de-seguridad-nota/",
+      "creator":"Andres Alejandro",
+      "pubDate":"Thu, 08 Jul 2021 11:15:00 +0000",
+      "description":"La falla de seguridad afecta a los sistemas operativos Windows 7 y Windows 10."
+    },
+    {
+      "title":"Cuáles son los mejores navegadores para usar en Internet",
+      "link":"https://eluniverso.com/larevista/tecnologia/cuales-son-los-mejores-navegadores-para-usar-en-internet-nota/",
+      "creator":"Tammy Armijos",
+      "pubDate":"Thu, 08 Jul 2021 19:52:12 +0000",
+      "description":"Google Chrome es el navegador más utilizado del mundo."
+    },
+    {
+      "title":"Desarrollan tecnología que alerta si los políticos están mirando el celular en las sesiones parlamentarias",
+      "link":"https://eluniverso.com/larevista/tecnologia/desarrollan-tecnologia-que-alerta-si-los-politicos-estan-mirando-el-celular-en-las-sesiones-parlamentarias-nota/",
+      "creator":"Andres Alejandro",
+      "pubDate":"Wed, 07 Jul 2021 19:17:27 +0000",
+      "description":"La herramienta desarrollada por Deporteer funciona de forma automática."
+    },
+    {
+      "title":"Twitter planea añadir notificaciones cuando otras personas buscan al usuario por su nombre",
+      "link":"https://eluniverso.com/larevista/tecnologia/twitter-planea-anadir-notificaciones-cuando-otras-personas-buscan-al-usuario-por-su-nombre-nota/",
+      "creator":"Andres Alejandro",
+      "pubDate":"Wed, 07 Jul 2021 19:05:41 +0000",
+      "description":"La plataforma ya cuenta con la opción de limitar que otras personas puedan encontrar al usuario buscando su número de teléfono o email."
+    },
+    {
+      "title":"Los hackers nos aventajan porque hay poca gente especializada en ciberseguridad. No damos abasto”: Soledad Antelada, la latina que protege al Departamento de Energía de EE.UU.",
+      "link":"https://eluniverso.com/larevista/tecnologia/los-hackers-nos-aventajan-porque-hay-poca-gente-especializada-en-ciberseguridad-no-damos-abasto-soledad-antelada-la-latina-que-protege-al-departamento-de-energia-de-eeuu-nota/",
+      "creator":"Carla Rodríguez",
+      "pubDate":"Wed, 07 Jul 2021 13:51:00 +0000",
+      "description":"Soledad Antelada Toledano se comporta como un hacker, pero de los “buenos”."
+    }
+     
+  ]
+
+
 
   ngOnInit(): void {
   }
