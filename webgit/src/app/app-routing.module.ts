3ec96d16b584adc063473f21e9c5bf278d2bf532
DarinkaTownsend <66121094+DarinkaTownsend@users.noreply.github.com>
2021-07-14 19:41:28 -0500

    servicios

--- webgit/src/app/app-routing.module.ts
+++ webgit/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { ContactosComponent } from './pages/contactos/contactos.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { EquipoDesarrolloComponent } from './pages/equipo-desarrollo/equipo-desarrollo.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
 const routes: Routes = [
   {path:"noticias", component:NoticiasComponent},
   {path:"contactos", component:ContactosComponent},
   {path:"equipo", component:EquipoDesarrolloComponent},
-  {path:"inicio", component:InicioComponent}
+  {path:"inicio", component:InicioComponent},
+  {path:"servicios", component:ServiciosComponent}
 ];
 
 @NgModule({
