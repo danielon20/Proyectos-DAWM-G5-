6990807f4409e29c9b2afc1a6c320dff6ff007bd
Raul Villao <rjvillao@espol.edu.ec>
2021-07-16 02:20:14 -0500

    Solucion
    Ultimos fallos

--- webgit/src/app/pages/inicio/inicio.component.ts
+++ webgit/src/app/pages/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class InicioComponent implements OnInit {
 
