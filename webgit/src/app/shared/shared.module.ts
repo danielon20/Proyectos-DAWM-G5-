11b3a2080aed3ae1b06898170c557603f7f89fa7
Raul Villao <rjvillao@espol.edu.ec>
2021-07-15 19:40:38 -0500

    Toolbar solución
    Solución del toolbar

--- webgit/src/app/shared/shared.module.ts
+++ webgit/src/app/shared/shared.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[HeaderComponent, FooterComponent]
+  exports:[HeaderComponent, FooterComponent,ToolbarComponent]
 })
 export class SharedModule { }
