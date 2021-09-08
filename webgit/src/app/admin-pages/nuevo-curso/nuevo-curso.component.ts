09464f15ddda0c20e1f67ca292481320970b5986
Raul Villao <rjvillao@espol.edu.ec>
2021-09-07 23:16:11 -0500

    Rutas de creacion de cursos
    Ruta de creacion de cursos y tareas

--- webgit/src/app/admin-pages/nuevo-curso/nuevo-curso.component.ts
+++ webgit/src/app/admin-pages/nuevo-curso/nuevo-curso.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../Interfacez-admin/curso';
+import { Router } from '@angular/router';
 // @ts-ignore
 
 @Component({
@@ -18,12 +19,68 @@ export class NuevoCursoComponent implements OnInit {
     ganancia:0,
   }
 
-  constructor() { }
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
 
   }
 
+  async submitMyCurso(){
+    //console.log((document.getElementById("nombreCurso")as HTMLInputElement).value);
+
+    let resultado = await fetch("http://localhost:3002/cursos",{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        nombre:(document.getElementById("nombreCurso")as HTMLInputElement).value,
+        precio:(document.getElementById("precioC")as HTMLInputElement).value,
+        descripcion:(document.getElementById("descC")as HTMLInputElement).value,
+      })
+    })
+    console.log(resultado)
+
+    let resultado2 = await fetch("http://localhost:3002/tareas",{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        titulo:(document.getElementById("nombre1")as HTMLInputElement).value,
+        subt:(document.getElementById("subtitulo1")as HTMLInputElement).value,
+        descripcion:(document.getElementById("info1")as HTMLInputElement).value,
+        linkf:(document.getElementById("eva1")as HTMLInputElement).value,
+        linkv:(document.getElementById("video1")as HTMLInputElement).value,
+      })
+    })
+    console.log(resultado2)
+
+    let resultado3 = await fetch("http://localhost:3002/tareas",{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        titulo:(document.getElementById("nombre2")as HTMLInputElement).value,
+        subt:(document.getElementById("subtitulo2")as HTMLInputElement).value,
+        descripcion:(document.getElementById("info2")as HTMLInputElement).value,
+        linkf:(document.getElementById("eva2")as HTMLInputElement).value,
+        linkv:(document.getElementById("video2")as HTMLInputElement).value,
+      })
+    })
+    console.log(resultado3)
+
+    let resultado4 = await fetch("http://localhost:3002/tareas",{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        titulo:(document.getElementById("nombre3")as HTMLInputElement).value,
+        subt:(document.getElementById("subtitulo3")as HTMLInputElement).value,
+        descripcion:(document.getElementById("info3")as HTMLInputElement).value,
+        linkf:(document.getElementById("eva3")as HTMLInputElement).value,
+        linkv:(document.getElementById("video3")as HTMLInputElement).value,
+      })
+    })
+    console.log(resultado4)
+
+    this.router.navigateByUrl("/admin/mis-cursos")
+  }
 
 
 
